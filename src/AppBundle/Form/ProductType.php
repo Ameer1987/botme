32e2cabbafa9fcc0e17d4d3e5fcbf47d9b926bda
Ameer1987 <ammeer_atteya@yahoo.com>
2019-08-02 17:59:31 +0200

    add the name field to the product form type

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -13,7 +13,7 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('code');
+        $builder->add('code')->add('name');
     }/**
      * {@inheritdoc}
      */

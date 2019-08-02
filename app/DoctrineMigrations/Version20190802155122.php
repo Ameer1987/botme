761b9b591c53b8850e83b9450aefdb0f5c95394c
Ameer1987 <ammeer_atteya@yahoo.com>
2019-08-02 17:53:06 +0200

    add the name field to the product entity add the code and name fields to the type entities

---
+++ app/DoctrineMigrations/Version20190802155122.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190802155122 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product ADD name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE item_type ADD code VARCHAR(40) NOT NULL, ADD name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE cart_type ADD code VARCHAR(40) NOT NULL, ADD name VARCHAR(255) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE cart_type DROP code, DROP name');
+        $this->addSql('ALTER TABLE item_type DROP code, DROP name');
+        $this->addSql('ALTER TABLE product DROP name');
+    }
+}

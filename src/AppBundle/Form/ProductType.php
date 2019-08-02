e0a07299198e2ae71f1e6ee97bed6511f5f646b0
Ameer1987 <ammeer_atteya@yahoo.com>
2019-08-02 23:36:49 +0200

    replace item type with product type

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -13,7 +13,7 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('code')->add('name');
+        $builder->add('code')->add('name')->add('type');
     }/**
      * {@inheritdoc}
      */

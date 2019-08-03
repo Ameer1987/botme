d5ee6494dc6555f9cb97f91fa71cf76f6100d0b2
Ameer1987 <ammeer_atteya@yahoo.com>
2019-08-03 17:07:25 +0200

    success message

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,3 +1,12 @@
 $(function () {
-
+    $(document).on('click', '.add-to-cart', function () {
+        $.ajax({
+            type: "POST",
+            url: $(this).data('url'),
+            success: function (data) {
+                $('#flash-msg').html(data).fadeIn('slow');
+                $('#flash-msg').delay(2000).fadeOut('slow');
+            }
+        })
+    });
 })

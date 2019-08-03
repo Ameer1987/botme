2959042ac1e62284ba2e1755c02fa897dbd65240
Ameer1987 <ammeer_atteya@yahoo.com>
2019-08-03 17:42:03 +0200

    loading ajax

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,11 +1,15 @@
 $(function () {
     $(document).on('click', '.add-to-cart', function () {
+        $('.modal').modal('show');
         $.ajax({
             type: "POST",
             url: $(this).data('url'),
             success: function (data) {
+                setTimeout(function () {
+                    $('.modal').modal('hide');
+                }, 1000);
                 $('#flash-msg').html(data).fadeIn('slow');
-                $('#flash-msg').delay(2000).fadeOut('slow');
+                $('#flash-msg').delay(1500).fadeOut('slow');
             }
         })
     });
